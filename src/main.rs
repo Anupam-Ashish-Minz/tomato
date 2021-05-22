ecc341e6b31a9a45bec9d32684c5842e8b5405cf
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-05-22 18:57:51 +0530

    tried added sound - failed

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
 use cli::parse_options;
 use timers::{ tomato, short_break, long_break };
+use sound::play_something;
 
 mod cli;
 mod timers; 
 mod notify;
+mod sound;
 
 fn main () {
-    let (start, sbreak, lbreak, watch) = parse_options();
-    if watch {
-        println!("will get the time");
-    } else {
-        if start {
-            tomato();
-        } if sbreak {
-            short_break();
-        } if lbreak {
-            long_break();
-        }
-    }
-    println!("end of funciton reached");
+    //let (start, sbreak, lbreak, watch) = parse_options();
+    //if watch {
+    //    println!("will get the time");
+    //} else {
+    //    if start {
+    //        tomato();
+    //    } if sbreak {
+    //        short_break();
+    //    } if lbreak {
+    //        long_break();
+    //    }
+    //}
+    //play_something();
+    //println!("end of funciton reached");
+    println!("start");
+    play_something();
+    println!("end");
 }
---
+++ src/sound.rs
@@ -0,0 +1,26 @@
+use libpulse_simple_binding::Simple;
+use libpulse_binding::stream::Direction;
+use libpulse_binding::sample::{ Spec, Format };
+
+pub fn play_something() {
+    let spec = Spec {
+        format: Format::S16NE,
+        channels: 2,
+        rate: 44100,
+    };
+    assert!(spec.is_valid());
+
+    let s = Simple::new(
+        None,                // Use the default server
+        "FooApp",            // Our application’s name
+        Direction::Playback, // We want a playback stream
+        None,                // Use the default device
+        "Music",             // Description of our stream
+        &spec,               // Our sample format
+        None,                // Use default channel map
+        None                 // Use default buffering attributes
+    ).unwrap();
+    let data: &mut [u8] = &mut [60];
+    let res = s.read(data);
+    println!("res {:?}", res);
+}
